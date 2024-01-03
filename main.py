5d8e87145dd8264feba72d630dedc40e4d789f4d
Basil Egupov <basilegupov@gmail.com>
2024-01-03 20:08:31 +0200

    v0.0.3 date.today()

--- main.py
+++ main.py
@@ -42,63 +42,20 @@ def get_birthdays_per_week(users):
 
 
 if __name__ == "__main__":
-    # users = [
-    #     {"name": "Natan Koum 23", "birthday": datetime(1976, 8, 30).date()},
-    #     {"name": "Mike Koum 23", "birthday": datetime(1976, 12, 30).date()},
-    #
-    #     {"name": "Greg Koum 23", "birthday": datetime(1971, 12, 31).date()},
-    #
-    #     {"name": "Jan Koum 24", "birthday": datetime(1976, 1, 1).date()},
-    #     {"name": "Gog Koum 24", "birthday": datetime(1971, 1, 2).date()},
-    #     {"name": "Mike Koum 24", "birthday": datetime(1976, 1, 3).date()},
-    #     {"name": "Kola Koum 24", "birthday": datetime(1976, 1, 4).date()},
-    #     {"name": "Pet Koum 24", "birthday": datetime(1976, 1, 5).date()},
-    #     {"name": "Jude Koum 24", "birthday": datetime(1976, 1, 6).date()},
-    # ]
-    today_ = datetime(2023, 12, 26)
     users = [
-        {
-            "name": "John",
-            "birthday": (today_ + timedelta(days=1)).date(),
-        },
-        {
-            "name": "Doe",
-            "birthday": (today_ + timedelta(days=3)).date(),
-        },
-        {"name": "Alice", "birthday": (today_ + timedelta(days=-3)).date()},
-    ]
-    result = get_birthdays_per_week(users)
-    print(result)
+        {"name": "Natan Koum 23", "birthday": datetime(1976, 8, 30).date()},
+        {"name": "Mike Koum 23", "birthday": datetime(1976, 12, 30).date()},
 
-    users = [
-        {
-            "name": "John",
-            "birthday": (today_ + timedelta(days=-5)).date(),
-        },
-        {
-            "name": "Doe",
-            "birthday": (today_ + timedelta(days=-6)).date(),
-        },
-        {
-            "name": "Alice",
-            "birthday": (datetime(2021, 1, 1)).date(),
-        },
-    ]
-    result = get_birthdays_per_week(users)
-    print(result)
+        {"name": "Greg Koum 23", "birthday": datetime(1971, 12, 31).date()},
 
-    users = [
-        {
-            "name": "John",
-            "birthday": (today_ + timedelta(days=5)).date(),
-        },
-        {
-            "name": "Doe",
-            "birthday": (today_ + timedelta(days=6)).date(),
-        },
-        {"name": "Alice", "birthday": (today_ + timedelta(days=3)).date()},
+        {"name": "Jan Koum 24", "birthday": datetime(1976, 1, 1).date()},
+        {"name": "Gog Koum 24", "birthday": datetime(1971, 1, 2).date()},
+        {"name": "Mike Koum 24", "birthday": datetime(1976, 1, 3).date()},
+        {"name": "Kola Koum 24", "birthday": datetime(1976, 1, 4).date()},
+        {"name": "Pet Koum 24", "birthday": datetime(1976, 1, 5).date()},
+        {"name": "Jude Koum 24", "birthday": datetime(1976, 1, 6).date()},
     ]
-    # print(users)
+
     result = get_birthdays_per_week(users)
     print(result)
     # Виводимо результат
